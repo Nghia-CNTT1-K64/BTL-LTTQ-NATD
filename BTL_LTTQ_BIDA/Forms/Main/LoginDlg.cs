ab06d7183aa8f6e489df436e3879ba2d863c8b1b
Hoang Van Nghia <nghiahoang14022005@gmail.com>
2025-10-28 15:55:14 +0700

    first

---
+++ BTL_LTTQ_BIDA/Forms/Main/LoginDlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_LTTQ_BIDA.Forms.Main
+{
+    public partial class LoginDlg : Form
+    {
+        public LoginDlg()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginDlg_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ptcLogoQuan_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
